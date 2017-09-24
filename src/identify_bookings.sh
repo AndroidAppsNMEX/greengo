eaf9b5e3ec627c08858a25b550d7f5d38a2b3b70
Carlos Rivas <carlos.rivas@king.com>
2017-09-24 12:06:50 +0200

    Added scripts

---
+++ src/identify_bookings.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+
+echo "Starting..."
+
+
+RUN_DATE=$1
+RUN_DATE_NO_DASH=$2
+
+echo "Populating WRK_BOOKINGS for $RUN_DATE..."
+bq query --allow_large_results --replace --destination_table poc_analytics.WRK_BOOKINGS\$${RUN_DATE_NO_DASH} --use_legacy_sql=false  --parameter=run_date:STRING:${RUN_DATE} "$(cat /home/hahavelka/greengo/src/SQL/WRK_BOOKINGS.sql)"
+
+echo "All Done..."
