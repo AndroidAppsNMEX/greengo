1e901f142302e7a1b273a4616d02e00342e7641a
AndroidAppsNMEX <crivas.itis@gmail.com>
2017-09-24 13:10:59 +0200

    Added ts

--- src/SQL/CLN_USER_ACTIVITIES.sql
+++ src/SQL/CLN_USER_ACTIVITIES.sql
@@ -4,7 +4,8 @@ SELECT
    CASE WHEN gps_latitude < 40 THEN gps_longitude ELSE gps_latitude END AS gps_latitude,
    CASE WHEN gps_latitude < 40 THEN gps_latitude ELSE gps_longitude END AS gps_longitude,
    SUBSTR(REPLACE(REPLACE(creation_date, '.', '-'), ' ', ''), 1, 10) as creation_date,
-   SUBSTR(REPLACE(REPLACE(creation_date, '.', '-'), ' ', ''), 12) as creation_hour
+   SUBSTR(REPLACE(REPLACE(creation_date, '.', '-'), ' ', ''), 12) as creation_hour,
+   DATETIME(timestamp(concat(SUBSTR(REPLACE(REPLACE(creation_date, '.', '-'), ' ', ''), 1, 10), ' ', SUBSTR(REPLACE(REPLACE(creation_date, '.', '-'), ' ', ''), 12)))) as creation_ts
 FROM poc_analytics.STG_USER_ACTIVITIES
 WHERE
   SUBSTR(REPLACE(REPLACE(creation_date, '.', '-'), ' ', ''), 1, 10) = @run_date
