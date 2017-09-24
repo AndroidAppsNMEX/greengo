ee027131dfece95beee8cd0a23ee21fbd060e804
AndroidAppsNMEX <crivas.itis@gmail.com>
2017-09-24 13:09:02 +0200

    Added creation_ts

--- src/SQL/CLN_VEHICLE_POSITION.sql
+++ src/SQL/CLN_VEHICLE_POSITION.sql
@@ -2,6 +2,7 @@ SELECT
       vehicle_id,
       SUBSTR(REPLACE(REPLACE(action_date, '.', '-'), ' ', ''), 1, 10) as action_date,
       SUBSTR(REPLACE(REPLACE(action_date, '.', '-'), ' ', ''), 12) as action_hour,
+      DATETIME(timestamp(concat(SUBSTR(REPLACE(REPLACE(creation_date, '.', '-'), ' ', ''), 1, 10), ' ', SUBSTR(REPLACE(REPLACE(creation_date, '.', '-'), ' ', ''), 12)))) as creation_ts,
       CASE WHEN gps_latitude < 40 THEN gps_longitude ELSE gps_latitude END AS gps_latitude,
       CASE WHEN gps_latitude < 40 THEN gps_latitude ELSE gps_longitude END AS gps_longitude,
       battery
