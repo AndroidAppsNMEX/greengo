269380c8ceb8d1744c7914eae53a2709d126485e
AndroidAppsNMEX <crivas.itis@gmail.com>
2017-09-24 13:09:50 +0200

    Added TS

--- src/SQL/CLN_RENTALS.sql
+++ src/SQL/CLN_RENTALS.sql
@@ -5,6 +5,8 @@ SELECT
         SUBSTR(REPLACE(REPLACE(booking_date, '.', '-'), ' ', ''), 12) as booking_hour,
         SUBSTR(REPLACE(REPLACE(end_date, '.', '-'), ' ', ''), 1, 10) as end_date,
         SUBSTR(REPLACE(REPLACE(end_date, '.', '-'), ' ', ''), 12) as end_hour,
+        DATETIME(timestamp(concat(SUBSTR(REPLACE(REPLACE(booking_date, '.', '-'), ' ', ''), 1, 10), ' ', SUBSTR(REPLACE(REPLACE(booking_date, '.', '-'), ' ', ''), 12)))) as booking_ts,
+        DATETIME(timestamp(concat(SUBSTR(REPLACE(REPLACE(end_date, '.', '-'), ' ', ''), 1, 10), ' ', SUBSTR(REPLACE(REPLACE(end_date, '.', '-'), ' ', ''), 12)))) as end_ts,
         status
       FROM poc_analytics.STG_RENTALS
    WHERE
